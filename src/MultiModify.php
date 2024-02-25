cd633f5cc1f9c68313916aa236958578237b4c5c
zhushu <zhushu@jshxl.cn>
2024-02-25 13:42:22 +0800

    first commit

---
+++ src/MultiModify.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Jshxl\MultiModify;
+
+use Laravel\Nova\Fields\Field;
+
+class MultiModify extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'multi-modify';
+}
